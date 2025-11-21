ae5b684eeb2dc233cda79e2bc48878412ad3a043
cuteducky <148722109+thecuteducky@users.noreply.github.com>
2025-11-21 13:45:46 +0100

    charac_names
    added_character names library

---
+++ notebooks/character_library.py
@@ -0,0 +1,38 @@
+# character_library.py
+
+# Character lists for both Dostoevsky novels
+CHARACTERS = {
+    "crime_and_punishment": {
+        "Raskolnikov": ["raskolnikov", "rodion", "rodya", "rodion romanovich"],
+        "Sonia": ["sonia", "sonya", "sofia semyonovna", "sofia marmeladov"],
+        "Marmeladov": ["marmeladov", "semyon zakharovich"],
+        "Katerina": ["katerina", "katerina ivanovna"],
+        "Dounia": ["dounia", "avdotya romanovna", "dunya"],
+        "Razumihin": ["razumihin", "razumikhin", "dmitri razumihin", "dmitri prokofitch"],
+        "Porfiry": ["porfiry", "porfiry petrovitch"],
+        "Svidrigailov": ["svidrigailov", "arkady ivanovich"],
+        "Luzhin": ["luzhin", "pyotr petrovitch"],
+        "Pulcheria": ["pulcheria", "pulcheria alexandrovna"],
+        "Lizaveta": ["lizaveta", "lizaveta ivanovna"],
+        "Alyona": ["alyona", "alyona ivanovna", "pawnbroker"],
+        "Zossimov": ["zossimov"],
+        "Nikodim": ["nikodim fomin"],
+    },
+
+    "brothers_karamazov": {
+        "Alyosha": ["alyosha", "alexei", "alexei fyodorovich"],
+        "Ivan": ["ivan", "ivan fyodorovich"],
+        "Dmitri": ["dmitri", "mitya", "dmitri fyodorovich"],
+        "Fyodor": ["fyodor", "fyodor pavlovich", "father karamazov"],
+        "Grushenka": ["grushenka", "agrafena alexandrovna"],
+        "Katerina": ["katerina", "katerina ivanovna"],
+        "Father Zosima": ["zosima", "elder zosima", "father zosima"],
+        "Smerdyakov": ["smerdyakov", "pavel", "pavel fyodorovich"],
+        "Rakitin": ["rakitin"],
+        "Kolya": ["kolya krasotkin"],
+        "Lise": ["lise", "lizaveta", "lizaveta snegiryevna"],
+        "Madame Khokhlakov": ["khokhlakov", "madame khokhlakov"],
+        "Ilusha": ["ilusha", "ilyusha snegiryev"],
+        "Captain Sneigiryov": ["captain sneigiryov", "sneigiryov"],
+    }
+}
